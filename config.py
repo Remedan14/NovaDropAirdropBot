c110bb35901060054d0709eca60c71d5fb4226cf
Remedan14 <remedanayub031@gmail.com>
2026-07-09 16:00:26 +0300

    Add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNELS = [
+    "@NovaDropAirdrop",
+    "@NovaDropAirdrop2"
+]
+
+REFERRAL_REWARD = 0.10
+
+MIN_WITHDRAW = 5.00
+
+ADMINS = [
+    123456789
+]
