6b4d8ca10c7a6cd04289af9abe4dbac01f748af5
Remedan14 <remedanayub031@gmail.com>
2026-07-10 15:52:19 +0300

    Update bot configuration parameters

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 import os
 
 # Telegram Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Required Telegram Channels
+# Telegram User ID kee
+ADMIN_ID = 8786492721
+
+# Mandatory channels
 CHANNELS = [
     "@NovaDropAirdrop",
     "@NovaDropAirdrop2"
 ]
 
-# Referral reward (USD)
-REFERRAL_REWARD = 0.10
+# Referral reward
+REFERRAL_REWARD = 0.50
 
-# Minimum withdrawal amount (USD)
+# Minimum withdrawal (USD)
 MIN_WITHDRAW = 5.00
 
 # Supported wallets
@@ -19,3 +23,6 @@ SUPPORTED_WALLETS = [
     "USDT (TRC20)",
     "Bitcoin"
 ]
+
+# Database
+DATABASE_NAME = "novadrop.db"
