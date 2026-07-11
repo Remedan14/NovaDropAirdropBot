fe1fec007f66336decdc7755e9c2baada383f4a2
Remedan14 <remedanayub031@gmail.com>
2026-07-12 02:19:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 """
-Telegram bot for NovaDrop Airdrop
-"""
-BOT_TOKEN"8627535451:AAGLeCr63Aug3UJJD9gq5HfygoG-3kqkqYU"
+Telegram bot for NovaDrop AirdqkqYU"
 import logging
 from typing import Optional
 
